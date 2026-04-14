0b6aadf7beddef60534815bc4ed17210f71b8f08
alexjt1917 <alejandro.juarezt@axity.com>
2026-04-13 19:22:54 -0600

    fix:coreción de formato de código de prueba de laboratorio de módulo 1, usando isort,black y ruff

---
+++ fundamental/modulo_01_entorno_herramientas/src/modulo_01_entorno_herramientas/app.py
@@ -0,0 +1,11 @@
+def saludar(nombre):
+    print(f"Hola, {nombre}")
+
+
+def suma(a, b):
+    return a + b
+
+
+if __name__ == "__main__":
+    saludar("Mundo")
+    print(suma(2, 3))
---
+++ fundamental/modulo_01_entorno_herramientas/tests/test_app.py
@@ -0,0 +1,5 @@
+from modulo_01_entorno_herramientas.app import suma
+
+
+def test_suma():
+    assert suma(2, 3) == 5
